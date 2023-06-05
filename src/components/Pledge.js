d45771a9f0f68299980cae3f56de129361e65623
tee <aderetioluwatoyin@gmail.com>
2023-06-05 13:22:03 +0100

    first commit

---
+++ src/components/Pledge.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import check from '../images/icon-check.svg'
+
+const Pledge = () => {
+  return (
+    <div>Pledge
+      <img className='check' src={check} alt='' />
+      <h1>hello</h1>
+
+    </div>
+  )
+}
+
+export default Pledge
