42fb59cdcfe58c8a832d055727471bf99742abe1
mharahyle <araile64@gmail.com>
2023-06-06 08:23:11 +0100

    routing fixes

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import About from './components/pages/About';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter >
+      
       <Navbar />
       <Routes>
         <Route  path='/pledge' element={<Pledge />} />
-        <Route  path='/about' element={<About/>} />
+        <Route  path='about' element={<About/>} />
 
 
 
       </Routes>
-      </BrowserRouter>
+    
     </div>
   );
 }
